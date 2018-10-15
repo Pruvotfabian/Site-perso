b5bcaad2d6aa1d02fc14ebae509dbf8b10704875
Pruvotfabian <pruvotfabian@gmail.com>
2018-10-15 16:29:07 +0200

    Ajout JS addevent; CSS effect;  key frames; test du sroll action

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,34 @@ var html, body;
 var concret = document.getElementById('concret');
 var fictif = document.getElementById('fictif');
 
+
+var circle = document.getElementById("circle");
+circle.addEventListener('click', click)
+function click() {
+  document.getElementById('test2').style.display = "block";
+  window.setTimeout(a, 200)
+  function a() {
+    document.getElementById('test').style.display = "block";
+    function extraire() {
+      character = message.substring(I, I=I+1) ;
+      if ( character == "-" &&  message.substr(I, 5) == "stop-" ) {              
+           character = "<br>" ;
+           I = I + 5;
+           }
+      test.innerHTML += character;
+      if ( I < message.length ) setTimeout(extraire, 100);
+      }
+ I = 0 ;
+ message = "-stop- Projet Story Teller V.1-stop- ";
+ extraire();
+  }
+  window.setTimeout(b, 2000)
+  function b() {
+    document.getElementById('test3').style.display = "block";
+  }
+  circle.removeEventListener('click', click);
+}
+
 window.onload = function() {
   //getting all anchor elements
   var links = document.links;
@@ -60,4 +88,5 @@ function scroll(from, to, hash) {
     }
   }, timeInterval);
   
+
 }
