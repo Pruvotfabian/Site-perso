6e85f4cc56080f64154ea3b4f81759dc48ca779c
Pruvotfabian <pruvotfabian@gmail.com>
2018-10-18 16:55:44 +0200

    Ajout de css finalisation des projets début de responsive

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,11 @@
-var html, body;
-var concret = document.getElementById('concret');
-var fictif = document.getElementById('fictif');
-
 
 var circle = document.getElementById("circle");
 circle.addEventListener('click', click)
 function click() {
+  document.getElementById('block_projet').style.width = "40%";
+  document.getElementById('width').style.maxWidth = "40%";
   document.getElementById('test2').style.display = "block";
+  
   window.setTimeout(a, 200)
   function a() {
     document.getElementById('test').style.display = "block";
@@ -42,6 +41,8 @@ function click2() {
 var circleb = document.getElementById("circleb");
 circleb.addEventListener('click', clickb)
 function clickb() {
+  document.getElementById('block_projetb').style.width = "40%";
+  document.getElementById('widthb').style.maxWidth = "40%";
   document.getElementById('test2b').style.display = "block";
   window.setTimeout(c, 300)
   function c() {
@@ -70,5 +71,123 @@ var trucb = document.getElementById("trucb");
 trucb.addEventListener('click', click3)
 function click3() {
    window.location = "https://github.com/Pruvotfabian/Site-de-la-Muerte";
+  
+}
+
+
+
+
+
+var circlec = document.getElementById("circlec");
+circlec.addEventListener('click', clickc)
+function clickc() {
+  document.getElementById('block_projetc').style.width = "40%";
+  document.getElementById('widthc').style.maxWidth = "40%";
+  document.getElementById('test2c').style.display = "block";
+  window.setTimeout(c, 300)
+  function c() {
+    document.getElementById('testc').style.display = "block";
+    function extraire() {
+      character = message.substring(I, I=I+1) ;
+      if ( character == "-" &&  message.substr(I, 5) == "stop-" ) {              
+           character = "<br>" ;
+           I = I + 5;
+           }
+      testc.innerHTML += character;
+      if ( I < message.length ) setTimeout(extraire, 100);
+      }
+ I = 0 ;
+ message = "-stop- Projet Titano -stop- ";
+ extraire();
+  }
+  window.setTimeout(d, 2000)
+  function d() {
+    document.getElementById('test3c').style.display = "block";
+  }
+  circle.removeEventListener('click', clickc);
+}
+
+var trucc = document.getElementById("trucc");
+trucc.addEventListener('click', click4)
+function click4() {
+   window.location = "https://github.com/Pruvotfabian/Site-de-la-Muerte";
+  
+}
+
+
+
+var circle1 = document.getElementById("circle1");
+circle1.addEventListener('click', click1)
+function click1() {
+  document.getElementById('block_projet1').style.width = "40%";
+  document.getElementById('width1').style.maxWidth = "40%";
+  document.getElementById('test21').style.display = "block";
+  
+  window.setTimeout(a, 200)
+  function a() {
+    document.getElementById('test1').style.display = "block";
+    function extraire() {
+      character = message.substring(I, I=I+1) ;
+      if ( character == "-" &&  message.substr(I, 5) == "stop-" ) {              
+           character = "<br>" ;
+           I = I + 5;
+           }
+      test1.innerHTML += character;
+      if ( I < message.length ) setTimeout(extraire, 100);
+      }
+ I = 0 ;
+ message = "-stop- Projet Osmose-stop- ";
+ extraire();
+  }
+  window.setTimeout(b, 2000)
+  function b() {
+    document.getElementById('test31').style.display = "block";
+  }
+  circle1.removeEventListener('click', click1);
+}
+
+var truc1 = document.getElementById("truc1");
+truc1.addEventListener('click', clicka)
+function clicka() {
+   window.location = "https://osmose-association.000webhostapp.com/";
+  
+}
+
+
+
+var circlez = document.getElementById("circle2");
+circlez.addEventListener('click', click5)
+function click5() {
+  document.getElementById('block_projet2').style.width = "40%";
+  document.getElementById('width2').style.maxWidth = "40%";
+  document.getElementById('test22').style.display = "block";
+  
+  window.setTimeout(a, 200)
+  function a() {
+    document.getElementById('test2a').style.display = "block";
+    function extraire() {
+      character = message.substring(I, I=I+1) ;
+      if ( character == "-" &&  message.substr(I, 5) == "stop-" ) {              
+           character = "<br>" ;
+           I = I + 5;
+           }
+      test2a.innerHTML += character;
+      if ( I < message.length ) setTimeout(extraire, 100);
+      }
+ I = 0 ;
+ message = "-stop- Projet Simplon -stop- ";
+ extraire();
+  }
+  window.setTimeout(b, 2000)
+  function b() {
+    document.getElementById('test32').style.display = "block";
+  }
+  circlez.removeEventListener('click', click5);
+}
+
+var truc2 = document.getElementById("truc2");
+truc2.addEventListener('click', clickz)
+function clickz() {
+   window.location = "http://calais.simplon.co/";
   
 }
