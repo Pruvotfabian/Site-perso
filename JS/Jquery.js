967c11f659e8e8304aa07f7f90bc4cda3e6caeb7
Pruvotfabian <pruvotfabian@gmail.com>
2018-10-19 16:21:20 +0200

    Ajout d'une nouvelle nav bar + scroll smouth css, ajout block de contact, Ajout jquery, image de fond, ajout du bouton CV + le CV

---
+++ JS/Jquery.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.toggle').toggleClass('active')
+    $('.sidebar').toggleClass('active')
+    
+  })
+})
+
+$(document).ready(function(){
+    $('.a').click(function(){
+      $('.toggle').removeClass('active')
+      $('.sidebar').removeClass('active')
+    })
+  })
